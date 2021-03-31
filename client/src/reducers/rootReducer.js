ddea19553cbdad5a14d0b0209e02caa16c0cfd3d
Muzaffer <aydogdum38@gmail.com>
2021-04-01 00:59:56 +0300

    added client side

---
+++ client/src/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import postReducer from "./post";
+
+
+const rootReducer = combineReducers({
+    post: postReducer,
+    
+});
+
+export default rootReducer;
